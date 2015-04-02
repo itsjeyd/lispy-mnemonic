71550c5a877909e906ec0e0ff8012452ab814c8d
Tim Krones <t.krones@gmx.net>
2015-04-02 13:58:17 +0200

    Add contents of README to "Commentary" section.

--- lispy-mnemonic.el
+++ lispy-mnemonic.el
@@ -27,13 +27,59 @@
 
 ;;; Commentary:
 ;;
-;; This package sets up mnemonic key bindings for Lispy.
+;; This package provides a set of mnemonic[1] key bindings for
+;; Lispy[2]. It also makes extensive use of hydras[3] for grouping
+;; related commands and improving discoverability.
 ;;
-;; See [5] for a full list of bindings.
-
+;; Background
+;;
+;; lispy-mnemonic started out as an attempt to make it easier for
+;; myself to learn Lispy. I'm familiar with the concept of modal
+;; editing, but I'm not a Vim user (which means that it doesn't come
+;; naturally to me to use, e.g., hjkl for movement). Also, I find it
+;; harder to commit a key sequence to memory if it is completely
+;; unrelated to the command it is bound to.
+;;
+;; Philosophy
+;;
+;; There are probably some exceptions but in general, key binding
+;; choices are based on the following principles:
+;;
+;; 1. *Make key bindings match command names.* Alternatively, base key
+;;    binding choices on default bindings for related functionality
+;;    that ships with Emacs.
+;;
+;; 2. *Group related commands* under a common prefix. Separate
+;;    commands that share a common prefix in Lispy if they don't seem
+;;    to be strongly related in terms of functionality.
+;;
+;; 3. *Improve discoverability*, but don't be overly obtrusive.
+;;
+;; 4. *Do not override default key bindings* (especially if the
+;;    functionality they provide is completely unrelated to the
+;;    command you are trying to bind).
+;;
+;; Target Audience
+;;
+;; If you:
+;;
+;; - would like to start learning Lispy
+;; - have played around with Lispy but not mastered it
+;; - haven't burned Vim-style key bindings into your muscle memory
+;; - find that mnemonics make it easier to learn and remember new
+;;   commands and key bindings
+;;
+;; ... there is a good chance you'll benefit from using lispy-mnemonic.
+;;
+;; If you have already memorized most or all of Lispy's commands and
+;; key bindings (kudos!), you probably don't need this package. But
+;; before you leave, *do* have a look at the hydras that
+;; lispy-mnemonic ships with[4]. Who knows, maybe you'll find a couple
+;; that you like :)
+;;
 ;; Installation
 ;;
-;; `lispy-mnemonic` is not on MELPA (yet). To start using it, follow
+;; lispy-mnemonic is not on MELPA[5] (yet). To start using it, follow
 ;; these steps:
 ;;
 ;; 1. If you haven't already, install Lispy:
@@ -48,14 +94,19 @@
 ;;
 ;;    (add-to-list 'load-path "~/path/to/lispy-mnemonic/")
 ;;    (require 'lispy-mnemonic)
-
-;;; Links
-
+;;
+;; Usage
+;;
+;; See [6] for a full list of bindings.
+;;
+;; Links
+;;
 ;; [1] https://en.wikipedia.org/wiki/Mnemonic
 ;; [2] https://github.com/abo-abo/lispy
-;; [3] http://melpa.org/
-;; [4] https://github.com/abo-abo/hydra
-;; [5] https://github.com/itsjeyd/lispy-mnemonic/blob/master/bindings.org
+;; [3] https://github.com/abo-abo/hydra
+;; [4] https://github.com/itsjeyd/lispy-mnemonic/blob/master/bindings.org#hydras
+;; [5] http://melpa.org/
+;; [6] https://github.com/itsjeyd/lispy-mnemonic/blob/master/bindings.org
 
 ;;; Code:
 
